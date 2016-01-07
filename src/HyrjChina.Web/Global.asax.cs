c4d736f4af9e5137a2e91ce472bdd0cd6155b174
liyang <535133951@qq.com>
2016-01-07 23:17:04 +0800

    3

---
+++ src/HyrjChina.Web/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using HyrjChina.Domain.Entities;
+using HyrjChina.Web.Infrastructure.Binders;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HyrjChina.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
+        }
+    }
+}
